ff09af86ac6f98c0b4e03b48cd61831dea25e665
Celjiii <marssolo123@gmail.com>
2023-11-14 12:08:11 +0700

    commit pertama

---
+++ lib/page/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green.shade300,
+        title: Text(
+          "HomePage",
+          style: TextStyle(color: Colors.white),
+        ),
+      ), //AppBar
+        body : Column(
+
+          children: [
+            Container(
+              margin: EdgeInsets.all(8),
+              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              decoration: BoxDecoration(
+                  color: Colors.green
+              ),
+              child: Text("Persegi"),
+            ),
+
+              Container(
+                margin: EdgeInsets.all(8),
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                decoration: BoxDecoration(
+                   color: Colors.green
+              ),
+              child: Text("Segitiga"),
+            )
+          ],
+     ),
+    );
+  }
+}
